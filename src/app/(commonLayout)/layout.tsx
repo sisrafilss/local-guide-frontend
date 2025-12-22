a531f5dd10ad58575ac561f7a8b0877a9b489cdf
Israfil Hossen <sisrafilss@gmail.com>
2025-12-22 08:01:44 +0600

    public navbar and footer added

---
+++ src/app/(commonLayout)/layout.tsx
@@ -0,0 +1,15 @@
+import PublicFooter from '@/components/shared/PublicFooter';
+import PublicNavbar from '@/components/shared/PublicNavbar';
+import React from 'react';
+
+const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <PublicNavbar />
+      {children}
+      <PublicFooter />
+    </>
+  );
+};
+
+export default CommonLayout;
